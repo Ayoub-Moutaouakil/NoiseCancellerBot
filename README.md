1f79e17904146756670cd198f6b90287ec1fc1de
Kashif Tasneem <kashift933@gmail.com>
2022-05-07 12:42:53 +0200

    Rattrapage PR. AYOUB
    Suppression de tout le travail pour le refaire

--- README.md
+++
@@ -1,2 +0,0 @@
-# NoiseCancellerBot
-Bot Discord qui exclue à partir d'un certain taux de décibel / Discord Bot which kicks someone when he makes to much noise
--- commands/meter.js
+++
@@ -1,9 +0,0 @@
-module.exports = async message => {
-// commande pour que le bot join le bon channel 
-const connection = await message.member.voice.channel.join;
-if(!connection){ // si problème de connection
-    return message.reply('VA DANS UN SALON GROS FDP'); // on répond
-}
-
-
-}
